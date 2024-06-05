63aa6da3c402e17f376a082276195839d8dd6deb
Oleksandra Nedashkivska <alexandranedashkovskaya10@gmail.com>
2024-06-05 15:36:42 +0300

    + call method fight in renderArena and add winner modal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,5 +1,10 @@
 import showModal from './modal';
+import { createFighterImage } from '../fighterPreview';
 
 export default function showWinnerModal(fighter) {
-    // call showModal function
+    const title = `Fighter ${fighter.name} wins!`;
+    const bodyElement = createFighterImage(fighter);
+    const onClose = () => window.location.reload();
+
+    showModal({ title, bodyElement, onClose });
 }
