4f164fda23b49920f2bc32acbd74a1a193510dd6
Oleksandra Nedashkivska <alexandranedashkovskaya10@gmail.com>
2024-06-05 16:29:55 +0300

    + body to winner modal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,9 +1,14 @@
 import showModal from './modal';
+import createElement from '../../helpers/domHelper';
 import { createFighterImage } from '../fighterPreview';
 
 export default function showWinnerModal(fighter) {
     const title = `Fighter ${fighter.name} wins!`;
-    const bodyElement = createFighterImage(fighter);
+
+    const bodyElement = createElement({ tagName: 'div', className: 'modal-body' });
+    const imageElement = createFighterImage(fighter);
+    bodyElement.append(imageElement);
+
     const onClose = () => window.location.reload();
 
     showModal({ title, bodyElement, onClose });
